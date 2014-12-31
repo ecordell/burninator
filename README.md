e2af90648c07bbb7d5b87c4a66930bcff0934fab
Evan Cordell <evan.cordell@localmed.com>
2014-12-31 15:30:10 -0500

    Adds coveralls

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-[![Build Status](https://travis-ci.org/ecordell/burninator.svg)](https://travis-ci.org/ecordell/burninator)
+[![Build Status](https://travis-ci.org/ecordell/burninator.svg)](https://travis-ci.org/ecordell/burninator) [![Coverage Status](https://img.shields.io/coveralls/ecordell/burninator.svg)](https://coveralls.io/r/ecordell/burninator)
 
 This is a test project to demonstrate packaging a Python project for release.
---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='burninator',
+    version='0.0.1',
+    description='Burnination Library for Python',
+    author='Evan Cordell',
+    author_email='cordell.evan@gmail.com',
+    url='',
+    license='MIT',
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    include_package_data=True,
+    install_requires=[
+        'colorama>=0.3.2',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ]
+)
